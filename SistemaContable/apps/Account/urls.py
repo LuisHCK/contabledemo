dc6f71968da2d7dea2096783b819dd8cb9d7680e
LuisHCK <luisjcenteno@live.com>
2017-06-27 01:05:35 -0600

    new route to list categories

--- SistemaContable/apps/Account/urls.py
+++ SistemaContable/apps/Account/urls.py
@@ -5,6 +5,7 @@ app_name = 'account'
 urlpatterns = [
     url(r'^$', views.getAccountsStructure, name='getAccountsStructure'),
     url(r'^account/$', views.listAccounts, name='listAccounts'),
+    url(r'^categories/$', views.listCategories, name='listCategories'),
     url(r'^account/new/$', views.newAccount, name='newAccount'),
     url(r'^account/(?P<pk>[0-9]+)/$', views.rudAccount,
         name='rudAccout'),
